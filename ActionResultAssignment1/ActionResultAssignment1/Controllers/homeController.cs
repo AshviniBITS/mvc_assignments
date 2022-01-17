b9e97181046b1bc80c0c6ebbce7f1d29321980e4
AshviniBITS <ashvinimeshram@benchmarkit.solutions>
2022-01-17 18:05:26 +0530

    First Day Assignment

---
+++ ActionResultAssignment1/ActionResultAssignment1/Controllers/homeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ActionResultAssignment1.Controllers
+{
+    public class homeController : Controller
+    {
+        // GET: home
+        public ActionResult Index(string arg)
+        {
+            if (arg == "sample")
+                return File("~/Sample.pdf", "application/pdf");
+            else if (arg == "gotoabout")
+                return RedirectToAction("About");
+            else if (arg == "login")
+                return View("Login");
+            else
+                return Content("You entered:" + arg);
+            
+        }
+        public ActionResult About()
+        {
+            return Content("about content here");
+
+        }
+    }
+}
---
+++ ActionResultAssignment1/ActionResultAssignment1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ActionResultAssignment1.MvcApplication" Language="C#" %>
