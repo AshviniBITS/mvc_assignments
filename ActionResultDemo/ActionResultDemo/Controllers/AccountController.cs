08d39e57ffbd39d271cacef3fe834e04b4232dc0
AshviniBITS <ashvinimeshram@benchmarkit.solutions>
2022-01-17 18:36:14 +0530

    Added first dy class practise demo

---
+++ ActionResultDemo/ActionResultDemo/Controllers/AccountController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ActionResultDemo.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login(string username,string password)
+        {
+            if(username=="admin" && password=="pass@123" )
+            {
+                return RedirectToAction("Dashboard", "Admin");
+            }
+            else
+            {
+                return RedirectToAction("InvalidLogin");
+            }
+            
+        }
+        public ActionResult InvalidLogin()
+        {
+            return View();
+        }
+        
+    }
+}
---
+++ ActionResultDemo/ActionResultDemo/Controllers/homeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ActionResultDemo.Controllers
+{
+    public class homeController : Controller
+    {
+        // GET: home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult GetEmpName(int Empid)
+        {
+            var employees = new[]
+            {
+                new{Empid=1,EmpName="Ash",salary=30000 },
+                 new
+                 {
+                     Empid = 2,
+                     EmpName = "Ashvini",
+                     salary = 40000 
+                 },
+
+
+                 
+                 new { Empid = 3, EmpName = "Abhi",salary = 20000 }
+                 
+            };
+            string matchEmpname = null;
+            foreach (var emp in employees)
+            {
+                if (emp.Empid==Empid)
+                {
+                    matchEmpname = emp.EmpName;
+                }
+            }
+            return Content(matchEmpname, "text/plain");
+
+
+        }
+        public ActionResult EmpFacebookLogin(int Empid)
+        {
+            string fburl = "https//www.facebook.com/emp" + Empid;
+            return Redirect(fburl);
+        }
+    }
+}
