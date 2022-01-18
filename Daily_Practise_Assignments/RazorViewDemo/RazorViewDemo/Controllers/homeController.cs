b455f80c123b6b1a1875a89171635a41fd2aa83d
AshviniBITS <ashvinimeshram@benchmarkit.solutions>
2022-01-18 15:19:36 +0530

    Adding 2nd day class work

---
+++ Daily_Practise_Assignments/RazorViewDemo/RazorViewDemo/Controllers/homeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RazorViewDemo.Controllers
+{
+    public class homeController : Controller
+    {
+        // GET: home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public  ActionResult StudentDetails()
+        {
+            ViewBag.StudentId = 101;
+            ViewBag.StudentName = "Ash";
+            ViewBag.marks = 70;
+            ViewBag.NoOfSem = 8;
+            ViewBag.Subject = new List<string>() { "maths", "chemistry", "physics" };
+            return View();
+
+        }
+    }
+}
