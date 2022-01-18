0074b4da5c4be221ac12c6efc6f767f89bc9131d
AshviniBITS <ashvinimeshram@benchmarkit.solutions>
2022-01-18 15:16:12 +0530

    Adding 2nd Day work

---
+++ CurrencyAssignment2/CurrencyAssignment2/Controllers/homeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CurrencyAssignment2.Controllers
+{
+    public class homeController : Controller
+    {
+        // GET: home
+        public ActionResult Index(int ?amount)
+        {
+            ViewBag.amount = amount;
+            return View();
+        }
+    }
+}
