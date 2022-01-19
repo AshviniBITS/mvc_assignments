137e0dbd349d5af72e8158b2a8cab2bee231f303
AshviniBITS <ashvinimeshram@benchmarkit.solutions>
2022-01-19 16:44:55 +0530

    Adding ViewBags Demo work

--- Daily_Practise_Assignments/MyFirstApp/MyFirstApp/Controllers/homeController.cs
+++ Daily_Practise_Assignments/MyFirstApp/MyFirstApp/Controllers/homeController.cs
@@ -13,5 +13,21 @@ namespace MyFirstApp.Controllers
         {
             return View();
         }
+        public ActionResult ResponseDemo()
+        {
+            Response.Write("hello from response example");
+            Response.ContentType = "text/plain";
+            return View();
+        }
+        public ActionResult RequestDemo()
+        {
+            ViewBag.Url = Request.Url;
+            ViewBag.PhysicalApplicationPath=Request.PhysicalApplicationPath;
+            ViewBag.path=Request.Path;
+            ViewBag.BrowserType = Request.Browser.Type;
+            ViewBag.QueryString=Request.QueryString["n"];
+            ViewBag.HttpMethod=Request.HttpMethod;
+            return View();
+        }
     }
 }
