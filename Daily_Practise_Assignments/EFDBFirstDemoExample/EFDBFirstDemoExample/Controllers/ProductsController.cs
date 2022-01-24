35a3e4cce08115c7203fa8549235e7added1a04f
AshviniBITS <ashvinimeshram@benchmarkit.solutions>
2022-01-24 15:15:52 +0530

    Seventh day work-DB First Approach-CRUD operaion

--- Daily_Practise_Assignments/EFDBFirstDemoExample/EFDBFirstDemoExample/Controllers/ProductsController.cs
+++ Daily_Practise_Assignments/EFDBFirstDemoExample/EFDBFirstDemoExample/Controllers/ProductsController.cs
@@ -13,10 +13,15 @@ namespace EFDBFirstDemoExample.Controllers
         public ActionResult Index(string search="")
         {
             EFDBFirstDatabaseEntities db = new EFDBFirstDatabaseEntities();
+
+            // List<Product>products=db.Products.Where(temp=>temp.CategoryId==1 && temp.Price>=30000).ToList();
+            /*List<Product> products = db.Products.Where(temp =>temp.ProductName.Contains(search)).ToList();
             ViewBag.search = search;
-           // List<Product>products=db.Products.Where(temp=>temp.CategoryId==1 && temp.Price>=30000).ToList();
-            List<Product> products = db.Products.Where(temp =>temp.ProductName.Contains(search)).ToList();
+            return View(products);*/
+            List<Product> products = db.Products.Where(temp => temp.ProductName.Contains(search)).ToList();
+            ViewBag.search = search;
             return View(products);
+
         }
         public ActionResult Details(long id)
         {
@@ -26,6 +31,10 @@ namespace EFDBFirstDemoExample.Controllers
         }
         public ActionResult create()
         {
+            EFDBFirstDatabaseEntities db = new EFDBFirstDatabaseEntities();
+            ViewBag.categories = db.Categories.ToList();
+            ViewBag.Brands = db.Brands.ToList();
+
             return View();
         }
         [HttpPost]
@@ -39,5 +48,48 @@ namespace EFDBFirstDemoExample.Controllers
 
 
         }
+        public ActionResult edit(long id)
+        {
+            EFDBFirstDatabaseEntities db = new EFDBFirstDatabaseEntities();
+            Product existingProduct = db.Products.Where(temp => temp.ProductId == id).FirstOrDefault();
+            ViewBag.categories = db.Categories.ToList();
+            ViewBag.Brands = db.Brands.ToList();
+            return View(existingProduct);
+        }
+        [HttpPost]
+        public ActionResult edit(Product p)
+        {
+            EFDBFirstDatabaseEntities db = new EFDBFirstDatabaseEntities();
+            Product existingProduct = db.Products.Where(temp => temp.ProductId == p.ProductId).FirstOrDefault();
+            existingProduct.ProductName = p.ProductName;
+            existingProduct.Price = p.Price;
+            existingProduct.DateOfPurchase = p.DateOfPurchase;
+            existingProduct.CategoryId = p.CategoryId;
+            existingProduct.BrandId = p.BrandId;
+            existingProduct.AvailabilityStatus = p.AvailabilityStatus;
+            existingProduct.Active = p.Active;
+            db.SaveChanges();
+
+            return RedirectToAction("Index","Products");
+        }
+        public ActionResult delete(long id)
+        {
+            EFDBFirstDatabaseEntities db = new EFDBFirstDatabaseEntities();
+            Product existingProduct = db.Products.Where(temp => temp.ProductId ==id).FirstOrDefault();
+            return View(existingProduct);
+
+        }
+        [HttpPost]
+        public ActionResult delete(long id,Product p)
+        {
+            EFDBFirstDatabaseEntities db = new EFDBFirstDatabaseEntities();
+            Product existingProduct = db.Products.Where(temp => temp.ProductId == id).FirstOrDefault();
+            db.Products.Remove(existingProduct);
+            db.SaveChanges();
+           
+            return RedirectToAction("Index", "Products");
+
+        }
+
     }
 }
