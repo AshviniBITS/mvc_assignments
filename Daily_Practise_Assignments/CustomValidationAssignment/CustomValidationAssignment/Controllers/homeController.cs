c44dbb9c29bef64edf783250f43eaf8711d685ea
AshviniBITS <ashvinimeshram@benchmarkit.solutions>
2022-01-28 17:32:56 +0530

    Adding custom validation in form created using Html helper classes

---
+++ Daily_Practise_Assignments/CustomValidationAssignment/CustomValidationAssignment/Controllers/homeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CustomValidationAssignment.Models;
+
+namespace CustomValidationAssignment.Controllers
+{
+    public class homeController : Controller
+    {
+        // GET: home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Registration()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Registration(User u)
+        {
+            if(ModelState.IsValid)
+            {
+                return Content("Your form has submitted successfully");
+            }
+            else
+            {
+                IEnumerable<string> allErrors = ModelState.Values.SelectMany(v => v.Errors).ToList().Select(error => error.ErrorMessage);  //convert list of error objects into a List<string>
+                return Content(string.Join(". ", allErrors));
+            }
+            
+        }
+    }
+}
---
+++ Daily_Practise_Assignments/CustomValidationAssignment/CustomValidationAssignment/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CustomValidationAssignment.CustomValidation;
+
+namespace CustomValidationAssignment.Models
+{
+    [HobbiesValidation(ErrorMessage="At least 1 and max 4 hobbies should be selected")]
+    public class User
+    {
+        public string name { get; set; }
+        public string email { get; set; }
+        public string Mobile { get; set; }
+        public bool Photography { get; set; }
+        public bool Gardening { get; set; }
+        public bool Dance { get; set; }
+        public bool Hiking { get; set; }
+        public bool Painting { get; set; }
+    }
+}
