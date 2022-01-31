48e1a4fff22db295d361cbac8f6be81e3bcacb7e
AshviniBITS <ashvinimeshram@benchmarkit.solutions>
2022-01-31 21:12:20 +0530

    Adding searching,sorting,pagination,Validations and Filters

--- Daily_Practise_Assignments/EFDBFirstDemoExample/EFDBFirstDemoExample/Controllers/ProductsController.cs
+++ Daily_Practise_Assignments/EFDBFirstDemoExample/EFDBFirstDemoExample/Controllers/ProductsController.cs
@@ -4,13 +4,28 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using EFDBFirstDemoExample.Models;
+using EFDBFirstDemoExample.filter;
 
 namespace EFDBFirstDemoExample.Controllers
 {
     public class ProductsController : Controller
     {
         // GET: Products
-        public ActionResult Index(string search="")
+        //public ActionResult Index(string search = "")
+        //{
+        //    EFDBFirstDatabaseEntities db = new EFDBFirstDatabaseEntities();
+
+        //    // List<Product>products=db.Products.Where(temp=>temp.CategoryId==1 && temp.Price>=30000).ToList();
+        //    /*List<Product> products = db.Products.Where(temp =>temp.ProductName.Contains(search)).ToList();
+        //    ViewBag.search = search;
+        //    return View(products);*/
+        //    List<Product> products = db.Products.Where(temp => temp.ProductName.Contains(search)).ToList();
+        //    ViewBag.search = search;
+        //    return View(products);
+
+        //}
+       
+        public ActionResult Index(string search = "",string SortColumn="ProductName",string IconClass="fa-sort-asc",int PageNo=1)
         {
             EFDBFirstDatabaseEntities db = new EFDBFirstDatabaseEntities();
 
@@ -20,15 +35,79 @@ namespace EFDBFirstDemoExample.Controllers
             return View(products);*/
             List<Product> products = db.Products.Where(temp => temp.ProductName.Contains(search)).ToList();
             ViewBag.search = search;
+            ViewBag.SortColumn = SortColumn;
+            ViewBag.IconClass = IconClass;
+            if(ViewBag.SortColumn=="ProductId")
+            {
+                if(ViewBag.IconClass=="fa-sort-asc")
+                    products=products.OrderBy(temp => temp.ProductId).ToList();
+                else
+                    products = products.OrderByDescending(temp => temp.ProductId).ToList();
+            }
+            if (ViewBag.SortColumn == "ProductName")
+            {
+                if (ViewBag.IconClass == "fa-sort-asc")
+                    products = products.OrderBy(temp => temp.ProductName).ToList();
+                else
+                    products = products.OrderByDescending(temp => temp.ProductName).ToList();
+            }
+            if (ViewBag.SortColumn == "Price")
+            {
+                if (ViewBag.IconClass == "fa-sort-asc")
+                    products = products.OrderBy(temp => temp.Price).ToList();
+                else
+                    products = products.OrderByDescending(temp => temp.ProductName).ToList();
+            }
+            if (ViewBag.SortColumn == "DateOfPurchase")
+            {
+                if (ViewBag.IconClass == "fa-sort-asc")
+                    products = products.OrderBy(temp => temp.DateOfPurchase).ToList();
+                else
+                    products = products.OrderByDescending(temp => temp.ProductName).ToList();
+            }
+            if (ViewBag.SortColumn == "AvailabilityStatus")
+            {
+                if (ViewBag.IconClass == "fa-sort-asc")
+                    products = products.OrderBy(temp => temp.AvailabilityStatus).ToList();
+                else
+                    products = products.OrderByDescending(temp => temp.ProductName).ToList();
+            }
+            if (ViewBag.SortColumn == "CategoryId")
+            {
+                if (ViewBag.IconClass == "fa-sort-asc")
+                    products = products.OrderBy(temp => temp.CategoryId).ToList();
+                else
+                    products = products.OrderByDescending(temp => temp.CategoryId).ToList();
+            }
+            if (ViewBag.SortColumn == "BrandId")
+            {
+                if (ViewBag.IconClass == "fa-sort-asc")
+                    products = products.OrderBy(temp => temp.BrandId).ToList();
+                else
+                    products = products.OrderByDescending(temp => temp.BrandId).ToList();
+            }
+            /*Pging*/
+            int NoOfRecordsPerPage=5;
+            int NoOfPages = Convert.ToInt32(Math.Ceiling(Convert.ToDouble(products.Count) /Convert.ToDouble(NoOfRecordsPerPage)));
+                int NoOfRecordsToSkip = (PageNo - 1) * NoOfRecordsPerPage;
+            ViewBag.PageNo = PageNo;
+            ViewBag.NoOfPages = NoOfPages;
+            products=products.Skip(NoOfRecordsToSkip).Take(NoOfRecordsPerPage).ToList();
+
+
+
             return View(products);
 
+
         }
+
         public ActionResult Details(long id)
         {
             EFDBFirstDatabaseEntities db = new EFDBFirstDatabaseEntities();
             Product p = db.Products.Where(temp => temp.ProductId == id).FirstOrDefault();
             return View(p);
         }
+        //[MyAuthanticationFilter]
         public ActionResult create()
         {
             EFDBFirstDatabaseEntities db = new EFDBFirstDatabaseEntities();
@@ -38,13 +117,31 @@ namespace EFDBFirstDemoExample.Controllers
             return View();
         }
         [HttpPost]
-        public ActionResult create(Product p)
+        public ActionResult create([Bind(Include ="ProductId,ProductName,Price,DataOfPurchase,AvailabilityStatus,CategoryId,BrabdId,Active,Photo")]Product p)
         {
             EFDBFirstDatabaseEntities db = new EFDBFirstDatabaseEntities();
-            db.Products.Add(p);
-            db.SaveChanges();
-            //return View();
-            return RedirectToAction("Index");
+            if (ModelState.IsValid)
+            {
+                if (Request.Files.Count >= 1)
+                {
+                    var file = Request.Files[0];
+                    var imgBytes = new Byte[file.ContentLength];
+                    file.InputStream.Read(imgBytes, 0, file.ContentLength);
+                    var Base64String = Convert.ToBase64String(imgBytes, 0, imgBytes.Length);
+                    p.Photo = Base64String;
+
+                }
+
+
+                db.Products.Add(p);
+                db.SaveChanges();
+                //return View();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
 
 
         }
--- Daily_Practise_Assignments/EFDBFirstDemoExample/EFDBFirstDemoExample/Models/Product.cs
+++ Daily_Practise_Assignments/EFDBFirstDemoExample/EFDBFirstDemoExample/Models/Product.cs
@@ -11,17 +11,27 @@ namespace EFDBFirstDemoExample.Models
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+    using EFDBFirstDemoExample.CustomValidation;
+
     public partial class Product
     {
+        [Required(ErrorMessage ="Product name cant be empty")]
         public long ProductId { get; set; }
+        [Required(ErrorMessage = "ProductId cant be empty")]
+        [RegularExpression(@"^[A-Za-z]*$",ErrorMessage ="Alphabets Only")]
+        [MaxLength(20,ErrorMessage="Product name should be max 20 character long")]
         public string ProductName { get; set; }
+        [DivisibilityBy10(ErrorMessage ="Price should be multiple of 10")]
         public Nullable<decimal> Price { get; set; }
         public Nullable<System.DateTime> DateOfPurchase { get; set; }
         public string AvailabilityStatus { get; set; }
+        [Required(ErrorMessage = "Category id cant be empty")]
         public Nullable<long> CategoryId { get; set; }
+        [Required(ErrorMessage = "Product name cant be empty")]
         public Nullable<long> BrandId { get; set; }
         public Nullable<bool> Active { get; set; }
+        public string Photo { get; set; }
     
         public virtual Brand Brand { get; set; }
         public virtual Category Category { get; set; }
